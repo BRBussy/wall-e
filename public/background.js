15a8650af7f8f9c2a23cc4ee7df0e97199cf14d9
bernardbussy <brbitzbussy@gmail.com>
2020-11-28 21:41:38 +0200

    make chrome specific

---
+++ public/background.js
@@ -0,0 +1,7 @@
+console.log('this be the background script')
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.storage.sync.set({color: '#3aa757'}, function () {
+        console.log("The color is green.");
+    });
+});
